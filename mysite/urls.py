ff9e5bba9130a26e25730461cd989497bb0c4a71
gabriel scalese <cc19171@g.unicamp.br>
2021-07-03 13:37:21 -0300

    Stopped tracking .env

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('API is running!')
 
 urlpatterns = [
+    path('', index), 
     path('api/', include('polls.api.urls')),
     path('admin/', admin.site.urls),
 ]
